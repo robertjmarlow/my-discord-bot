d3b65fc55944b06d276a2dabafdc09b18ba86f33
robertjmarlow <robertjmarlow@gmail.com>
2025-02-19 21:42:33 -0600

    It...works?

---
+++ src/js/deploy-commands.js
@@ -0,0 +1,59 @@
+import { REST, Routes } from 'discord.js';
+import { readdirSync } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import 'dotenv/config';
+
+// Grab all the command folders from the commands directory you created earlier
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const foldersPath = join(__dirname, 'commands');
+const commandFolders = readdirSync(foldersPath);
+
+async function getCommands() {
+	const commands = [];
+
+	for (const folder of commandFolders) {
+		// Grab all the command files from the commands directory you created earlier
+		const commandsPath = join(foldersPath, folder);
+		const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+		for (const file of commandFiles) {
+			console.log(file);
+			const filePath = join(commandsPath, file);
+			await import(filePath).then((command) => {
+				if ('data' in command && 'execute' in command) {
+					commands.push(command.data.toJSON());
+				} else {
+					console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+				}
+			});
+		}
+	}
+
+	return commands;
+}
+
+const commands = await getCommands();
+
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.token);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.clientId, process.env.guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,68 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'node:fs';
+import { join, dirname } from 'node:path';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { fileURLToPath } from 'node:url';
 import 'dotenv/config';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const foldersPath = join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+async function getCommands() {
+    const commands = new Collection();
+
+    for (const folder of commandFolders) {
+        const commandsPath = join(foldersPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const filePath = join(commandsPath, file);
+            // const command = require(filePath);
+			await import(filePath).then((command) => {
+				// Set a new item in the Collection with the key as the command name and the value as the exported module
+				if ('data' in command && 'execute' in command) {
+					commands.set(command.data.name, command);
+				} else {
+					console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+				}
+			});
+        }
+    }
+
+    return commands;
+}
+
+client.commands = await getCommands();
+
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+	
+    console.log(interaction);
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
 
 client.login(process.env.token);
